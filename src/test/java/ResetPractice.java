dadb07619a5f410afaee228c73d3e8aabda56a4c
Bashir Hasanov <beshir.hasanov@gmail.com>
2021-05-22 12:41:24 -0400

    work 4 is done after resetting

--- src/test/java/ResetPractice.java
+++ src/test/java/ResetPractice.java
@@ -1,12 +1,2 @@
 public class ResetPractice {
-<<<<<<< HEAD
-    // work 4 is done
-
-    // work 5 is done
-
-    // work 6 is done
-
-    // committing all together
-=======
->>>>>>> 3d0b4d4 (work 3 - created a new class after reset)
 }
